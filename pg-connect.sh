d7485e098fd53f5af96f19f57d5153fbd3688e01
Andrew Dryga <andrew@dryga.com>
2017-05-31 18:04:23 +0300

    Improved pg connection helpers

--- pg-connect.sh
+++ pg-connect.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 # This script provides easy way to connect to remote postgres DB.
 # Example: ./bin/pg-connect.sh -n mp -l app=postgres
-set -e
+set -em
 
-# Trap exit so we can try to kill proxies that has stuck in background
-function cleanup {
-  echo " - Stopping kubectl port forwarding."
-  kill $! &> /dev/null
-}
-trap cleanup EXIT
-
-K8S_SELECTOR="app=postgres"
+K8S_SELECTOR="app=postgresql"
+RUN_PSQL="false"
 
 # Read configuration from CLI
-while getopts "n:l:c:" opt; do
+while getopts "n:l:qc:" opt; do
   case "$opt" in
     n)  K8S_NAMESPACE="--namespace=${OPTARG}"
         ;;
     l)  K8S_SELECTOR=${OPTARG}
+        ;;
+    q)  RUN_PSQL="true"
         ;;
   esac
 done
@@ -28,13 +24,42 @@ if [ ! $K8S_SELECTOR ]; then
   exit 1
 fi
 
-POD=$(kubectl get pods -l ${K8S_SELECTOR} ${K8S_NAMESPACE} \
+POD_NAME=$(kubectl get pods -l ${K8S_SELECTOR} ${K8S_NAMESPACE} \
   -o template --template="{{range.items}}{{.metadata.name}}{{end}}")
 
-if [ ! $POD ]; then
+if [ ! ${POD_NAME} ]; then
   echo "[E] Pod wasn't found. Try to select it with -n (namespace) and -l options."
   exit 1
 fi
 
-echo "Found pod ${POD}."
-kubectl ${K8S_NAMESPACE} port-forward ${POD} 5433:5432
+echo " - Found pod ${POD_NAME}."
+if [[ "${RUN_PSQL}" == "true" ]]; then
+  echo " - Attaching to a psql CLI on remote host (type '\q' to exit)."
+  kubectl exec ${K8S_NAMESPACE} ${POD_NAME} -it -- /bin/sh -c 'psql -U ${POSTGRES_USER} ${POSTGRES_DB}'
+else
+  # Trap exit so we can try to kill proxies that has stuck in background
+  function cleanup {
+    echo " - Stopping ktl port forwarding."
+    kill $! &> /dev/null
+    kill %1 &> /dev/null
+  }
+  trap cleanup EXIT
+
+  echo " - Resolving DB user."
+  POSTGRES_USER=$(kubectl get pod ${POD_NAME} ${K8S_NAMESPACE} -o jsonpath='{$.spec.containers[0].env[?(@.name=="POSTGRES_USER")].value}')
+  echo " - Resolving DB password."
+  POSTGRES_PASSWORD=$(kubectl get pod ${POD_NAME} ${K8S_NAMESPACE} -o jsonpath='{$.spec.containers[0].env[?(@.name=="POSTGRES_PASSWORD")].value}')
+  echo " - Resolving DB name."
+  POSTGRES_DB=$(kubectl get pod ${POD_NAME} ${K8S_NAMESPACE} -o jsonpath='{$.spec.containers[0].env[?(@.name=="POSTGRES_DB")].value}')
+
+  echo " - Port forwarding remote PostreSQL to port 5433."
+  kubectl ${K8S_NAMESPACE} port-forward ${POD_NAME} 5433:5432 &
+
+  sleep 1
+
+  echo " - Open PG CLI: open postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5433/${POSTGRES_DB}"
+  open "postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5433/${POSTGRES_DB}"
+
+  echo " - Returning control over port-forward"
+  fg
+fi;
