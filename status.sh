c08bb9d8a62da041d6eb100a082294eaa6b8c72e
Andrew Dryga <andrew@dryga.com>
2017-06-01 18:22:59 +0300

    WIP: Status script

--- status.sh
+++ status.sh
@@ -6,8 +6,9 @@ function get_cluser_versions() {
     jq --raw-output '[ .items[] | select( .metadata.namespace != "" and .metadata.namespace != "kube-system" and .metadata.namespace != "monitoring" and .metadata.labels.app != "postgresql" ) | { app: .metadata.labels.app, version: .metadata.labels.version, pod_name: .metadata.name, container: .spec.containers[0].image | split(":")[0], ns: .metadata.namespace } ]'
 }
 
-function get_docker_hub_token() {
-  curl --silent "https://auth.docker.io/token?service=registry.docker.io&scope=$1" | jq -r .token
+function get_docker_hub_tags() {
+  DOCKER_HUB_TOKEN=$(curl --silent "https://auth.docker.io/token?service=registry.docker.io&scope=$1" | jq -r .token)
+  curl -H "Content-Type: application/json" -H "Authorization: Bearer ${DOCKER_HUB_TOKEN}" https://registry.hub.docker.com/v2/$1/tags/list -Ss
 }
 
 function get_version_on_k8s_local() {
@@ -15,23 +16,20 @@ function get_version_on_k8s_local() {
   ruby -ryaml -e "puts YAML.load(File.read('$path'))['metadata']['labels']['version']"
 }
 
-
-CLUSTER_VERSIONS=$(get_cluser_versions)
-
-# Get token to be able to talk to Docker Hub
-DOCKER_HUB_SCOPES=$(echo ${CLUSTER_VERSIONS} | jq 'map(.container) | "repository%3A" + join("%3Apull+repository%3A") + "%3Apull"')
-# DOCKER_HUB_SCOPES=${DOCKER_HUB_SCOPES//\//%2F}
-
 # "Application\tContainer\tDeployed version\tPod Name\n"$1}'
-get_cluser_versions | \
-  jq -r '.[] | "\(.app)\t\(.container)\t\(.version)\t\(.pod_name)" ' | \
-  column -t
-
+get_cluser_versions | jq -r '.[] | "\(.app)\t\(.container)\t\(.version)\t\(.pod_name)"' | \
 while read key
 do
-  curl -Ss "$RIAK/buckets/$BUCKET/keys/$key?keys"
+  echo $key
+  DOCKER_HUB_TOKE=$(echo ${key} | awk '{print $2}')
+  echo $DOCKER_HUB_TOKE
+  echo $(get_docker_hub_tags $DOCKER_HUB_TOKE)
 done
 
+# echo ${CLUSTER_VERSIONS}
+
+
+# | column -t
 
 # DOCKER_HUB_TOKEN=$(get_docker_hub_token repository:nebo15/man_api:pull)
 # curl -H "Content-Type: application/json" -H "Authorization: Bearer ${DOCKER_HUB_TOKEN}" https://registry.hub.docker.com/v2/nebo15/man_api/tags/list -v # &last=last_tag
