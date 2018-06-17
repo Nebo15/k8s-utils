3251cd6c7a35c809bfff5363100e23446c35c21e
Andrew Dryga <andrew@dryga.com>
2018-06-17 18:29:38 +0300

    Revive some commands

--- shell.sh
+++ shell.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
-# This script provides easy way to connect to remote shell.
-# Example: ./bin/shell.sh -n mp -l app=postgres
 set -e
 
+function show_help {
+  echo "
+  ktl shell -lSELECTOR [-nNAMESPACE -h /bin/sh]
+
+  Connects to the shell of a random pod selected by label and namespace.
+
+  By default it runs /bin/sh.
+
+  Examples:
+    ktl shell -lapp=hammer-web               Connect to one of the pods of hammer-web application in default namespace.
+    ktl shell -lapp=hammer-web -nweb         Connect to one of the pods of hammer-web application in web namespace.
+    ktl shell -lapp=hammer-web /bin/bash     Connect to one of the pods of hammer-web application and run /bin/bash.
+"
+}
+
+K8S_NAMESPACE=""
+
 # Read configuration from CLI
-while getopts "n:l:c:" opt; do
+while getopts "n:l:h" opt; do
   case "$opt" in
     n)  K8S_NAMESPACE="--namespace=${OPTARG}"
         ;;
     l)  K8S_SELECTOR=${OPTARG}
         ;;
+    c)  COMMAND=${OPTARG}
+        ;;
+    h)  show_help
+        exit 0
+        ;;
   esac
 done
+shift $(expr $OPTIND - 1 )
+COMMAND=$1
+
+if [[ "${COMMAND}" == "" ]]; then
+  COMMAND="/bin/sh"
+fi
 
 # Required part of config
 if [ ! $K8S_SELECTOR ]; then
@@ -19,13 +45,17 @@ if [ ! $K8S_SELECTOR ]; then
   exit 1
 fi
 
-POD=$(kubectl get pods -l ${K8S_SELECTOR} ${K8S_NAMESPACE} \
-  -o template --template="{{range.items}}{{.metadata.name}}{{end}}")
+POD_NAME=$(
+  kubectl get pods ${K8S_NAMESPACE} \
+    -l ${K8S_SELECTOR} \
+    -o jsonpath='{.items[0].metadata.name}' \
+    --field-selector=status.phase=Running
+)
 
-if [ ! $POD ]; then
-  echo "[E] Pod wasn't found. Try to select it with -n (namespace) and -l options."
+if [[ "${POD_NAME}" == "" ]]; then
+  echo "[E] Pod wasn't found. Try to select it with -n [namespace] and -l [selector] options."
   exit 1
 fi
 
-echo "Found pod ${POD}."
-kubectl exec ${K8S_NAMESPACE} ${POD} -it /bin/sh
+echo "Found pod ${POD_NAME}."
+kubectl exec ${K8S_NAMESPACE} ${POD_NAME} -it -- ${COMMAND}
--- status.sh
+++ status.sh
@@ -1,38 +1,51 @@
 #!/bin/bash
 # This script backups all critical data, allowing to move it from one environment to another
+echo "Creating temporarely Docker Hub token to pull list of tags"
+DOCKER_REGISTRY="https://index.docker.io/v1/"
+DOCKER_CREDENTIALS=$(echo "${DOCKER_REGISTRY}" | docker-credential-osxkeychain get)
+DOCKER_USERNAME=$(echo "${DOCKER_CREDENTIALS}" | jq -r .Username)
+DOCKER_PASSWORD=$(echo "${DOCKER_CREDENTIALS}" | jq -r .Secret)
+PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
+KUBECTL_CONTEXT=$(kubectl config current-context)
 
 function get_cluser_versions() {
-  kubectl get pods --all-namespaces=true --output=json | \
-    jq --raw-output '[ .items[] | select( .metadata.namespace != "" and .metadata.namespace != "kube-system" and .metadata.namespace != "monitoring" and .metadata.labels.app != "postgresql" ) | { app: .metadata.labels.app, version: .metadata.labels.version, pod_name: .metadata.name, container: .spec.containers[0].image | split(":")[0], ns: .metadata.namespace } ]'
+  kubectl get deployments \
+    --all-namespaces=true \
+    --output=json | \
+    jq --raw-output '[
+      .items[] | select( .metadata.namespace != "kube-system" and .metadata.namespace != "monitoring" and .metadata.labels.app != "db" ) | { app: .metadata.labels.app, version: .spec.template.spec.containers[0].image | split(":")[1], container: .spec.template.spec.containers[0].image | split(":")[0], ns: .metadata.namespace }
+    ]'
 }
 
 function get_docker_hub_tags() {
-  TOKEN=$(curl --silent "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$1:pull" | jq -r .token)
-  curl -Ss -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" "https://registry.hub.docker.com/v2/$1/tags/list?n=100"
+  DOCKER_HUB_TOKEN=$(curl -u${DOCKER_USERNAME}:${DOCKER_PASSWORD} --silent "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$1:pull" | jq -r .token)
+  curl -Ss -H "Content-Type: application/json" -H "Authorization: Bearer ${DOCKER_HUB_TOKEN}" "https://registry.hub.docker.com/v2/$1/tags/list?n=100"
 }
 
-function get_version_on_k8s_local() {
-  CONFIG_PATH="./clusters/api/$1/api/dep.yaml"
-  ruby -ryaml -e "puts YAML.load(File.read('$path'))['metadata']['labels']['version']"
+function get_manifest_version() {
+  VALUES_PATH="${PROJECT_ROOT_DIR}/rel/deployment/charts/$1/values.$2.yaml"
+  [[ -e "${VALUES_PATH}" ]] && cat "${VALUES_PATH}" | grep "imageTag" | awk '{print $NF;}' | sed 's/"//g' || echo "Unknown"
 }
 
 echo "Loading cluster status (this may take a while).."
 
-# "Application\tContainer\tDeployed version\tPod Name\n"$1}'
 get_cluser_versions | \
-  jq -r '.[] | "\(.ns)/\(.app)\t\(.container)\t\(.version)\t\(.pod_name)"' | \
+  jq -r '.[] | "\(.ns)|\(.app)|\(.container)|\(.version)"' | \
   while read key
   do
-    REPO_SLUG=$(echo ${key} | awk '{print $2}')
-    CURRENT_VERSION=$(echo ${key} | awk '{print $3}')
+    REPO_SLUG=$(echo ${key} | awk -F "|" '{print $3}')
+    CURRENT_VERSION=$(echo ${key} | awk -F "|" '{print $4}')
     LATEST_VERSION=$(get_docker_hub_tags "${REPO_SLUG}" "${CURRENT_VERSION}" | jq --raw-output '([.tags[] | scan("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")] | max_by(split(".") | (.[0] | tonumber) * 1000000 + (.[1] | tonumber) * 10000 + (.[2] | tonumber)))')
+    STAGING_VALUES_VERSION=$(get_manifest_version $(echo ${key} | awk -F "|" '{print $2}') "staging")
+    PRODUCTION_VALUES_VERSION=$(get_manifest_version $(echo ${key} | awk -F "|" '{print $2}') "production")
 
-    # echo -n "." >&2
-    echo -e ${key}'\t'${LATEST_VERSION}
+    echo -e ${key}'|'${LATEST_VERSION}'|'${STAGING_VALUES_VERSION}'|'${PRODUCTION_VALUES_VERSION}
   done | \
-  awk -v red="$(tput setaf 1)" -v white="$(tput setaf 7)" -v reset="$(tput sgr0)" 'BEGIN {printf "%sApp%s\t%sImage%s\t%sDeployed_version%s\t%sLatest_version%s\t%sPod_name%s\n", white, reset, white, reset, white, reset, white, reset, white, reset} {if ($3 != $5) color=red; else color=white; printf "%s%s%s\t%s%s%s\t%s%s%s\t%s%s%s\t%s%s%s\n", white, $1, reset, white, $2, reset, color, $3, reset, white, $5, reset, white, $4, reset;}' | \
-  column -t
-
-
-
-
+  awk \
+     -F "|" \
+    -v red="$(tput setaf 1)" \
+    -v white="$(tput setaf 7)" \
+    -v reset="$(tput sgr0)" \
+    -v context=${KUBECTL_CONTEXT} \
+    'BEGIN {printf "Namespace|App|Image|On %s|Latest version|Staging values version|Production values version\n", context} {printf "%s|%s|%s|%s|%s|%s|%s\n", $1, $2, $3, $4, $5, $6, $7;}' | \
+  column -t -s'|'
