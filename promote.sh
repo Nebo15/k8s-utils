5d79eea3f18ca23879ac1c71ac89d634bf7c293d
Andrew Dryga <andrew@dryga.com>
2019-08-18 14:46:37 +0300

    Fix promote to use values.yaml as production values file

--- promote.sh
+++ promote.sh
@@ -38,17 +38,26 @@ done
 PROJECT_ROOT_DIR=$(git rev-parse --show-toplevel)
 APPLICATIONS_DIR="${PROJECT_ROOT_DIR}/rel/deployment/charts/applications"
 
+function values_path() {
+  if [[ "$2" == "production" ]]; then
+    echo "${APPLICATIONS_DIR}/$1/values.yaml"
+  else
+    echo "${APPLICATIONS_DIR}/$1/values.$2.yaml"
+  fi
+}
+
 function get_manifest_version() {
-  VALUES_PATH="${APPLICATIONS_DIR}/$1/values.$2.yaml"
+  VALUES_PATH=$(values_path $1 $2)
   [[ -e "${VALUES_PATH}" ]] && cat "${VALUES_PATH}" | grep "imageTag" | awk '{print $NF;}' | sed 's/"//g' || echo ""
 }
 
 function promote() {
   FROM_VERSION=$(get_manifest_version $1 $3)
   TO_VERSION=$(get_manifest_version $1 $2)
+  VALUES_PATH=$(values_path $1 $3)
 
   if [[ "${FROM_VERSION}" != "" && "${TO_VERSION}" != "" ]]; then
-    sed -E -i '' 's#(imageTag:[ ]*"[^"]*"[ ]*)#imageTag: "'${TO_VERSION}'"#' "${APPLICATIONS_DIR}/$1/values.$3.yaml"
+    sed -E -i '' 's#(imageTag:[ ]*"[^"]*"[ ]*)#imageTag: "'${TO_VERSION}'"#' "${VALUES_PATH}"
     echo "[I] Promoted $1 ${FROM_VERSION} -> ${TO_VERSION}"
   elif [[ "${FROM_VERSION}" == "" ]]; then
     echo "[W] Skipping $1 app because it have no configuration for $3 environment"
